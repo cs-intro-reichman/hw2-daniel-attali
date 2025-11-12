9de8f2bc5d9b5688239adc11b98ed40b88f741af
Daniel ATTALI <danielattali@MacBook-Pro-de-Daniel.local>
2025-11-12 22:55:40 +0200

    finieshed

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,14 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
-	}
+	     int n = Integer.parseInt(args[0]);
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            double sign = (i % 2 == 0) ? 1.0 : -1.0;
+            sum += sign * (1.0 / (2 * i + 1));
+        }
+        double piApprox = 4 * sum;
+        System.out.println("pi according to Java: " + Math.PI);
+        System.out.println("pi, approximated:     " + piApprox);
+    }
 }
--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,23 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String word = args[0];
+            int times = Integer.parseInt(args[1]);
+            word=word.toUpperCase();
+            String an ="AEFHILMNORSX";
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                boolean usean=(an.indexOf(ch)>=0);
+                String article = usean ? "an " : "a  ";
+                System.out.println("Give me " + article + " " + ch + ": " + ch + "!");
+            }
+
+            System.out.println("What does that spell?");
+            for (int i = 0; i < times; i++) {
+            System.out.println(word + "!!!");
+}
+
+
+
         }
 }
