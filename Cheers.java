318b81628121ae514a1ec326c34d9ea87cc0ebf0
Daniel ATTALI <danielattali@MacBook-Pro-de-Daniel.local>
2025-11-12 23:05:12 +0200

    adjusted

--- Cheers.java
+++ Cheers.java
@@ -1,22 +1,26 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    String word = args[0];
-            int times = Integer.parseInt(args[1]);
-            word=word.toUpperCase();
-            String an ="AEFHILMNORSX";
-            for (int i = 0; i < word.length(); i++) {
-                char ch = word.charAt(i);
-                boolean usean=(an.indexOf(ch)>=0);
-                String article = usean ? "an " : "a  ";
-                System.out.println("Give me " + article + " " + ch + ": " + ch + "!");
-            }
+	   String wordInput = args[0];
+                String upperWordInput = wordInput.toUpperCase();
+                int numTimes = Integer.parseInt(args[1]);
+                
+                for (int i = 0; i < upperWordInput.length(); i++) {
+                        
+                        char singleWord = upperWordInput.charAt(i); 
+                        String specialWord = "AEFHILMNORSX"; 
+                        if (specialWord.indexOf(singleWord) >= 0) {
+                                System.out.println("Give me an " + singleWord + ": " + singleWord + "!");
+                        } else {
+                                 System.out.println("Give me a  " + singleWord + ": " + singleWord + "!");
+                        }
+                }
 
-            System.out.println("What does that spell?");
-            for (int i = 0; i < times; i++) {
-            System.out.println(word + "!!!");
-}
+                System.out.println("What does that spell?");
 
+                for (int j = 0; j < numTimes; j++) {
+                        System.out.println(upperWordInput + "!!!");
+                }
 
 
         }
